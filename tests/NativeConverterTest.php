3554740213c3cea4926408ab487f9226ba7f48f0
Toon Van den Bos <toon@whitecube.be>
2017-06-01 11:32:01 +0200

    Added NativeConverterTest

---
+++ tests/NativeConverterTest.php
@@ -0,0 +1,85 @@
+<?php
+
+use \WhiteCube\Lingua\Service as Lingua;
+use PHPUnit\Framework\TestCase;
+
+class NativeConverterTest extends TestCase
+{
+
+    /** @test */
+    public function can_be_created_from_native()
+    {
+        $this->assertInstanceOf(WhiteCube\Lingua\Service::class, Lingua::createFromNative('български език'));
+    }
+
+    /** @test */
+    public function cannot_be_created_from_invalid_native()
+    {
+        $this->expectException(\Exception::class);
+        Lingua::createFromNative('test');
+    }
+
+    /** @test */
+    public function can_convert_native_to_name()
+    {
+        $language = Lingua::createFromNative('български език');
+        $this->assertEquals('bulgarian', $language->toName());
+    }
+
+    /** @test */
+    public function can_convert_native_to_iso_639_1()
+    {
+        $language = Lingua::createFromNative('shqip');
+        $this->assertEquals('sq', $language->toISO_639_1());
+    }
+
+    /** @test */
+    public function can_convert_native_to_iso_639_2t()
+    {
+        $language = Lingua::createFromNative('shqip');
+        $this->assertEquals('sqi', $language->toISO_639_2t());
+    }
+
+    /** @test */
+    public function can_convert_native_to_iso_639_2b()
+    {
+        $language = Lingua::createFromNative('shqip');
+        $this->assertEquals('alb', $language->toISO_639_2b());
+    }
+
+    /** @test */
+    public function can_convert_native_to_iso_639_3()
+    {
+        $language = Lingua::createFromNative('shqip');
+        $this->assertEquals('sqi + 4', $language->toISO_639_3());
+    }
+
+    /** @test */
+    public function can_convert_native_to_w3c_iso_639_1()
+    {
+        $language = Lingua::createFromNative('shqip');
+        $this->assertEquals('sq', $language->toW3C());
+    }
+
+    /** @test */
+    public function can_convert_native_to_w3c_and_fallback_to_iso_639_3()
+    {
+        $language = Lingua::createFromNative('tachelhit');
+        $this->assertEquals('tzm', $language->toW3C());
+    }
+
+    /** @test */
+    public function can_convert_native_to_php_iso_639_1()
+    {
+        $language = Lingua::createFromNative('shqip');
+        $this->assertEquals('sq', $language->toPHP());
+    }
+
+    /** @test */
+    public function can_convert_native_to_php_and_fallback_to_iso_639_3()
+    {
+        $language = Lingua::createFromNative('tachelhit');
+        $this->assertEquals('tzm', $language->toPHP());
+    }
+
+}
